f49f20cbb8b53471fb12c40b80db9339d47ee109
Camilo Martinez <cambmppbc@gmail.com>
2021-05-21 20:16:23 -0500

    Commit inicial

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License
 
-Copyright (c) 2021 tayloRswift authors
+Copyright (c) 2021 shakiRa authors
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- R/colors.R
+++ R/colors.R
@@ -1,28 +1,29 @@
 #' Color palettes
 #'
 #' @export
-swift_palettes <- list(
-  `taylorSwift` = c("#61b6cc", "#577f3f" ,"#e3e9f3", "#0a1605","#fddac7","#81a757"),
-  `fearless` = c("#b68f51","#5b3617","#f7eabe", "#ecd59f","#825c2d"),
-  `speakNow` = c("#4b2671","#5e291c", "#f3d8c4","#f3bf73","#ffffff"),
-  `speakNowLive` = c("#fce178", "#969696", "#871d20", "#090708", "#fafaf9"),
-  `taylorRed` = c("#c2c2ae","#26233b","#7f6557", "#b4a382","#eeeadf"),
-  `taylor1989` = c("#b1532a","#84697f","#cbb593","#a88f92","#e8eadf", "#43475b"),
-  `reputation` = c("#060606","#6e6e6e","#fefefe","#cacaca", "#060606","#8c8c8c"),
-  `lover` = c("#b8396b","#ffd1d7","#fff5cc","#76bae0","#b28f81","#54483e"),
-  `folklore` = c("#272727","#5c5c5c","#bababa", "#f8f8f8"),
-  `evermore` = c("#efefef","#827d73", "#3d2620", "#e89264","#474247")
+shakira_palettes <- list(
+  `magia` = c("#79ABDC", "#9FBB8B" ,"#C26151", "#E95485","#F4DC6A","#F2A569"),
+  `peligro` = c("#D1A5B2","#B98184","#E93F37", "#714346","#423134"),
+  `piesDescalzos` = c("#9A5071","#ECCDB1","#653739", "#C1832E","#83856D"),
+  `dondeEstanLosLadrones` = c("#C56789","#744356", "#B63530","#E7B89E","#251919"),
+  `servicioDeLavanderia` = c("#DEBB77", "#C87153", "#9D7458", "#2C211D", "#8F502F"),
+  `fijacionOralVol1` = c("#B22E29","#501115","#BC9388", "#CEC1C2","#C29674"),
+  `fijacionOralVol2` = c("#3A5042","#D1A37F","#39261E","#71323A","#E7D4AF"),
+  `loba` = c("#72C1C8","#EEBD92","#519F86","#CF9762", "#CF9762","#1A0622"),
+  `saleElSol` = c("#835174","#FCE7BB","#DD9386","#C95771"),
+  `shakira` = c("#21285A", "#F7F2EF", "#CE8679", "#A3617C"),
+  `elDorado` = c("#DEC9B1","#EEE4DA","#C8A68A", "#D8C582", "#B07A6E")
 )
 
 
 #' Function to interpolate a color palette
 #'
-#' @param palette Character name of palette in tswift_palettes
+#' @param palette Character name of palette in shakira_palettes
 #' @param reverse Boolean true if palette should be reversed
 #' @param ... Additional arguments to pass to colorRampPalette()
 #' @export
-swift_pal <- function(palette = "taylor1989", reverse = FALSE, ...){
-  pal <- swift_palettes[[palette]]
+shakira_pal <- function(palette = "dondeEstanLosLadrones", reverse = FALSE, ...){
+  pal <- shakira_palettes[[palette]]
 
   if(reverse){
     pal <- rev(pal)
@@ -31,39 +32,39 @@ swift_pal <- function(palette = "taylor1989", reverse = FALSE, ...){
   grDevices::colorRampPalette(pal, ...)
 }
 
-#' Color scale for Taylor Swift colors
+#' Color scale for Shakira colors
 #'
-#' @param palette Character name of palette in swift_palettes
+#' @param palette Character name of palette in shakira_palettes
 #' @param discrete Boolean if color aesthetic is discrete
 #' @param reverse Boolean indicating whether palette should be reversed
 #' @param ... Additional arguments used to discrete_scale() or scale_fill_gradientn()
 #'   to automatically interpolate between colours.
 #' @export
-scale_color_taylor <- function(palette = "taylor1989",
+scale_color_shakira <- function(palette = "dondeEstanLosLadrones",
                                discrete = TRUE, reverse=FALSE,...){
-  pal <- swift_pal(palette = palette, reverse = reverse)
+  pal <- shakira_pal(palette = palette, reverse = reverse)
 
   if(discrete){
-    ggplot2::discrete_scale("colour", paste0("swift_", palette), palette = pal, ...)
+    ggplot2::discrete_scale("colour", paste0("shakira_", palette), palette = pal, ...)
   }else{
     ggplot2::scale_color_gradientn(colours = pal(256), ...)
   }
 }
 
-#' Fill scale for Taylor Swift colors
+#' Fill scale for Shakira colors
 #'
-#' @param palette Character name of palette in swift_palettes
+#' @param palette Character name of palette in shakira_palettes
 #' @param discrete Boolean if color aesthetic is discrete
 #' @param reverse Boolean if palette should be reversed
 #' @param ... Additional arguments used to discrete_scale() or scale_fill_gradientn()
 #'   to automatically interpolate between colours.
 #' @export
-scale_fill_taylor <- function(palette = "taylor1989",
+scale_fill_shakira <- function(palette = "dondeEstanLosLadrones",
                               discrete = TRUE, reverse = FALSE, ...){
-  pal <- swift_pal(palette = palette, reverse = reverse)
+  pal <- shakira_pal(palette = palette, reverse = reverse)
 
   if(discrete){
-    ggplot2::discrete_scale("fill", paste0("swift_", palette), palette = pal, ...)
+    ggplot2::discrete_scale("fill", paste0("shakira_", palette), palette = pal, ...)
   }else{
     ggplot2::scale_fill_gradientn(colours = pal(256),...)
   }
